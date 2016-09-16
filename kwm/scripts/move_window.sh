90355cf7d26726ca47be488ce82d4e10bdc7bc8b
koekeishiya <aasvi93@hotmail.com>
2016-09-17 01:26:46 +0200

    add missing $

--- kwm/scripts/move_window.sh
+++ kwm/scripts/move_window.sh
@@ -1,6 +1,6 @@
 kwmc=/usr/local/bin/kwmc
 DIR=$1
-FOCUSED_WINDOW=$(kwmc query window focused id)
+FOCUSED_WINDOW=$($kwmc query window focused id)
 NEW_WINDOW=$($kwmc query window focused "$DIR")
 SAME_PARENT=$($kwmc query window parent "$FOCUSED_WINDOW" "$NEW_WINDOW")
 if [ "$SAME_PARENT" = "true" ]
