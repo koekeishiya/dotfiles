f155ec28abb73166f2b64779d4a3288315d5f058
koekeishiya <aasvi93@hotmail.com>
2016-12-25 17:35:28 +0100

    update hyperterm

--- hyperterm/hyper.js
+++ hyperterm/hyper.js
@@ -1,16 +1,15 @@
 module.exports = {
   config: {
-
     // hyperclean makes the window borderless and we want to hide tabs
-    hyperclean: {
-        hideTabs: true
-    },
+    // hyperclean: {
+        // hideTabs: true
+    // },
 
     // default font size in pixels for all tabs
     fontSize: 10,
 
     // font family with optional fallbacks
-    fontFamily: 'Input Mono, Courier New',
+    fontFamily: '"Input Mono", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(127,127,127,0.60)',
@@ -22,13 +21,13 @@ module.exports = {
     foregroundColor: '#d5c4a1',
 
     // terminal background color
-    backgroundColor: '#1b1b1b',
+    backgroundColor: '#1f1f1f',
 
     // border color (window, tabs)
     borderColor: 'rgba(0,0,0,0)',
 
     // custom css to embed in the main window
-    css: ' ',
+    css: '',
 
     // custom css to embed in the terminal window
     termCSS: `
@@ -37,12 +36,41 @@ module.exports = {
       }
     `,
 
+    // set to `true` if you're using a Linux set up
+    // that doesn't shows native menus
+    // default: `false` on Linux, `true` on Windows (ignored on macOS)
+    showHamburgerMenu: '',
+
+    // set to `false` if you want to hide the minimize, maximize and close buttons
+    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
+    // default: `true` on windows and Linux (ignored on macOS)
+    showWindowControls: '',
+
     // custom padding (css format, i.e.: `top right bottom left`)
     padding: '15px 15px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
+    colors: {
+      black: '#1f1f1f',
+      red: '#d75f5f',
+      green: '#b8bb26',
+      yellow: '#af865a',
+      blue: '#535c5c',
+      magenta: '#775759',
+      cyan: '#6d715e',
+      white: '#c0b18b',
+      lightBlack: '#4a3637',
+      lightRed: '#d75f5f',
+      lightGreen: '#7b8748',
+      lightYellow: '#af865a',
+      lightBlue: '#535c5c',
+      lightMagenta: '#775759',
+      lightCyan: '#6d715e',
+      lightWhite: '#c0b18b',
+    },
+    /*
     colors: {
       black: '#1b1b1b',
       red: '#d75f5f',
@@ -61,12 +89,29 @@ module.exports = {
       lightCyan: '#00ffff',
       lightWhite: '#504945',
     },
+    */
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
-    shell: ''
+    shell: '/bin/zsh',
 
-    // for advanced config flags please refer to https://hyperterm.org/#cfg
+    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used
+    shellArgs: ['--login'],
+
+    // for environment variables
+    env: {},
+
+    // set to false for no bell
+    bell: 'SOUND',
+
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: false
+
+    // URL to custom bell
+    // bellSoundURL: 'http://example.com/bell.mp3',
+
+    // for advanced config flags please refer to https://hyper.is/#cfg
   },
 
   // a list of plugins to fetch and install from npm
@@ -76,12 +121,12 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-               "hyperclean",
-           ],
+    "hyperminimal",
+    "hyper-simple-vibrancy",
+  ],
 
   // in development, you can create a directory under
-  // `~/.hyperterm_plugins/local/` and include it here
+  // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
   localPlugins: []
 };
-
