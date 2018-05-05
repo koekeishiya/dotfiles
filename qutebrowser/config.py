996f6f980e0d45d4ceceb6faf913e93ac917cf02
koekeishiya <aasvi93@hotmail.com>
2018-05-05 12:26:25 +0200

    update qutebrowser, add launch script, update skhdrc

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -20,12 +20,16 @@ config.set('content.javascript.enabled', True, 'qute://*/*')
 
 # Padding (in pixels) around text for tabs.
 # Type: Padding
-c.tabs.padding = {'bottom': 0, 'left': 5, 'right': 5, 'top': 5}
+c.tabs.padding = {'bottom': 5, 'left': 5, 'right': 5, 'top': 5}
 
 # Page to open if :open -t/-b/-w is used without URL. Use `about:blank`
 # for a blank page.
 # Type: FuzzyUrl
 c.url.default_page = 'https://koekeishiya.github.io/chunkwm/'
+
+# Page(s) to open at the start.
+# Type: List of FuzzyUrl, or FuzzyUrl
+c.url.start_pages = 'https://koekeishiya.github.io/chunkwm/'
 
 # Hide the window decoration.  This setting requires a restart on
 # Wayland.
@@ -60,6 +64,28 @@ c.colors.statusbar.command.fg = '#d4c5a1'
 # Background color of the statusbar in command mode.
 # Type: QssColor
 c.colors.statusbar.command.bg = '#202020'
+
+# Default foreground color of the URL in the statusbar.
+# Type: QssColor
+c.colors.statusbar.url.fg = '#d5c4a1'
+
+# Foreground color of the URL in the statusbar on error.
+# Type: QssColor
+c.colors.statusbar.url.error.fg = '#d75f5f'
+
+# Foreground color of the URL in the statusbar on successful load
+# (http).
+# Type: QssColor
+c.colors.statusbar.url.success.http.fg = '#84edb9'
+
+# Foreground color of the URL in the statusbar on successful load
+# (https).
+# Type: QssColor
+c.colors.statusbar.url.success.https.fg = '#8fee96'
+
+# Foreground color of the URL in the statusbar when there's a warning.
+# Type: QssColor
+c.colors.statusbar.url.warn.fg = '#cd950c'
 
 # Background color of the tab bar.
 # Type: QtColor
---
+++ scripts/qtb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# initial idea: Florian Bruhin (The-Compiler)
+# author: Thore Bödecker (foxxx0)
+
+_proto_version=1
+_ipc_socket=$(ls $(echo $TMPDIR)qutebrowser/ipc-*)
+_qute_bin="/Applications/qutebrowser.app/Contents/MacOS/qutebrowser"
+
+printf '{"args": [":open -w"], "target_arg": null, "protocol_version": %d, "cwd": "%s"}\n' \
+   "${_proto_version}" \
+   "${PWD}" | socat - UNIX-CONNECT:"${_ipc_socket}" 2>/dev/null || "$_qute_bin" "$@" &
