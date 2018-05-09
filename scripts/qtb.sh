8c57add509afaa1b64801c5ca3e6d1bfb64e3d9e
koekeishiya <aasvi93@hotmail.com>
2018-05-09 19:16:12 +0200

    qtb.sh: fix disown through open

--- scripts/qtb.sh
+++ scripts/qtb.sh
@@ -3,9 +3,8 @@
 # author: Thore Bödecker (foxxx0)
 
 _proto_version=1
-_ipc_socket=$(ls $(echo $TMPDIR)qutebrowser/ipc-*)
-_qute_bin="/Applications/qutebrowser.app/Contents/MacOS/qutebrowser"
+_ipc_socket=$(ls $(echo $TMPDIR)qutebrowser/ipc-* 2>/dev/null)
 
 printf '{"args": [":open -w"], "target_arg": null, "protocol_version": %d, "cwd": "%s"}\n' \
    "${_proto_version}" \
-   "${PWD}" | socat - UNIX-CONNECT:"${_ipc_socket}" 2>/dev/null || "$_qute_bin" "$@" 2>/dev/null &
+   "${PWD}" | socat - UNIX-CONNECT:"${_ipc_socket}" 2>/dev/null || open -a qutebrowser.app 2>/dev/null
